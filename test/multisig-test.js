0f2e630431bdaa9249d1087a5a5704f0aff23370
dmfxyz <d@dmf.xyz>
2022-07-10 18:42:57 -0400

    make and use helper functions for domain separator and transaction hash

--- test/multisig-test.js
+++ test/multisig-test.js
@@ -46,23 +46,11 @@ TestHarness.test('msig moves tokens', {
     const r_arr = []
     const s_arr = []
 
-    let domain_data = EIP712DOMAINTYPE_HASH
-                   + NAME_HASH.slice(2) 
-                   + VERSION_HASH.slice(2)
-                   + utils.hexlify(chain_id).slice(2).padStart(64, '0')
-                   + multisig.address.slice(2).padStart(64, '0')
-                   + SALT.slice(2)
-    domain_data = domain_data.toLowerCase()
-    DOMAIN_SEPARATOR = utils.keccak256(domain_data)
+    const DOMAIN_SEPARATOR = createDomainSeparator(EIP712DOMAINTYPE_HASH, NAME_HASH, VERSION_HASH, 
+        chain_id, multisig.address, SALT)
     
-    let tx_input = TXTYPE_HASH
-                + rico.address.slice(2).padStart(64, '0')
-                + utils.hexlify(eth_amt).slice(2).padStart(64, '0')
-                + utils.keccak256(data).slice(2)
-                + utils.hexlify(nonce).slice(2).padStart(64, '0')
-                + executor.slice(2).padStart(64, '0')
-    tx_input = tx_input.toLowerCase()
-    let tx_input_hash = utils.keccak256(tx_input)
+    let tx_input_hash = createTransactionHash(TXTYPE_HASH, rico.address, eth_amt, data, nonce, 
+        executor)
     
     let input = '0x19' + '01' + DOMAIN_SEPARATOR.slice(2) + tx_input_hash.slice(2)
     let msg_hash = utils.keccak256(input)
@@ -84,7 +72,7 @@ TestHarness.test('msig moves tokens', {
     assert.equal(bob_rico_2.sub(rico_amt).eq(bob_rico_1), true)
 })
 
-TestHarness.test('insufficient members', {
+TestHarness.test('insufficient member signatures', {
 }, async (harness, assert) => {
 
 })
@@ -141,3 +129,26 @@ TestHarness.test('fail create member addresses wrong order', {
         assert.equal(e.reason, "VM Exception while processing transaction: reverted with reason string 'Order err'")
     }
 })
+
+// Test Helper Functions 
+function createDomainSeparator(domain_type_hash, name_hash, version_hash, chain_id, address, salt) {
+    // TODO: !DMFXYZ! Probably useful to have this as a seperate import
+    // and not rely on this files constants
+    const domain_data = domain_type_hash
+                   + name_hash.slice(2) 
+                   + version_hash.slice(2)
+                   + utils.hexlify(chain_id).slice(2).padStart(64, '0')
+                   + address.slice(2).padStart(64, '0')
+                   + salt.slice(2)
+    return utils.keccak256(domain_data.toLowerCase())
+}
+
+function createTransactionHash(tx_type_hash, target_address, amount, data, nonce, executor) {
+     let tx_input = tx_type_hash
+                + target_address.slice(2).padStart(64, '0')
+                + utils.hexlify(amount).slice(2).padStart(64, '0')
+                + utils.keccak256(data).slice(2)
+                + utils.hexlify(nonce).slice(2).padStart(64, '0')
+                + executor.slice(2).padStart(64, '0')
+    return utils.keccak256(tx_input.toLowerCase())
+}
